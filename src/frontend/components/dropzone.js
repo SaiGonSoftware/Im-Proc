2fa016a7905dcc6bf087b7ee7206d7dd5b8ad0d9
Harley <suong.liang711@gmailc.om>
2021-02-06 21:50:32 +0700

    add progress bar
    add progress bar for img upload

--- src/frontend/components/dropzone.js
+++ src/frontend/components/dropzone.js
@@ -2,20 +2,25 @@ import React, { useCallback, useEffect, useState } from 'react'
 import { useDropzone } from 'react-dropzone'
 import style from '../styles/Dropzone.module.css';
 import {guidGenerator} from '../helpers/guidGenerator';
+import ProgressBar from'../components/progressBar';
 
 export default function MyDropzone() {
 
-  const [state, setState] = useState({ images: [], userLoadMoreItem: false });
+  const [state, setState] = useState({ images: [], isUploading: false , percentage: 0});
 
   const setImgsIntoState = (imagesArray) => {
-    setState({
-      ...state,
-      images: [...state.images, ...imagesArray]
-    });
+    setTimeout( () => {
+      setState({
+        ...state,
+        images: [...state.images, ...imagesArray],
+        isUploading: false,
+        percentage: 100
+      });
+    }, 500);
   };
-
+ 
   const onDrop = (acceptedFiles) => {
-    setState({ ...state, userLoadMoreItem: true });
+    setState({ ...state, isUploading: true, percentage: acceptedFiles && acceptedFiles.length > 1 ? Math.round(100 / acceptedFiles.length) : 50 });
     let newImages = acceptedFiles.map(file => Object.assign(file, {
       preview: URL.createObjectURL(file),
       guid: guidGenerator()
@@ -43,6 +48,7 @@ export default function MyDropzone() {
         <input {...getInputProps()} />
         <p>Drag 'n' drop some files here, or click to select files</p>
       </div>
+      {state.isUploading ? <ProgressBar style={style} isUploading={state.isUploading} percentage={state.percentage}/> : <div>Please upload an image</div>}
       <div className={style.imageWrapper}>
         {state.images.length > 0 && (state.images.map((img, index) => (
           <div key={index} className={style.imageItem}>
